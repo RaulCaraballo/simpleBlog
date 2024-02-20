939d2aa45355d688d6d4f6da0466ec085c896ca5
unknown <ulraul11ca@gmail.com>
2024-02-20 17:08:48 +0100

    Se ha cambiado el estilo por completo y se han añadido un par de cambios en registration.php

--- views/registration.php
+++ views/registration.php
@@ -14,30 +14,50 @@
         <div id="content">
             <div id="welcomeMsg" class="element left">
                 <h1>
-                    Bienvenido!
+                    ¡Bienvenido!
                 </h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores dolore est eum eveniet
                     itaque perferendis porro quo sed sunt.</p>
             </div>
             <div id="loginForm" class="element right">
                 <form action="../controllers/RegistrationController.php" method="post" id="mainForm">
+                
+                    <label for="nombre" name="nombre" id="nombre">
+                        Nombre
+                        <br>
+                        <input type="nombre" name="nombre" id="nombre" value="">
+                    </label>
+                    <br>
+                    <label for="apellidos" name="apellidos" id="apellidos">
+                        Apellidos
+                        <br>
+                        <input type="apellidos" name="apellidos" id="apellidos" value="">
+                    </label>
+                    <br>
+                    <label for="ciudad" name="ciudad" id="ciudad">
+                        Ciudad
+                        <br>
+                        <input type="ciudad" name="ciudad" id="ciudad" value="">
+                    </label>
+                    <br>
                     <label for="login">
-                        email
+                        Correo electrónico
                         <br>
                         <input type="email" name="email" id="login" value="">
                     </label>
                     <br>
                     <label for="password">
-                        Password
+                        Contraseña
                         <br>
                         <input type="password" name="password" id="password" value="">
                     </label>
                     <br>
+                    
                     <label for="submit">
                         <input type="submit" name="submit" value="Sign Up" id="submitData">
                     </label>
                     <label for="register">
-                        <a href="login.php">Ya tienes cuenta?</a>
+                        <a href="login.php">¿Ya tienes cuenta?</a>
                     </label>
                 </form>
             </div>
