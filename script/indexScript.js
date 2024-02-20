70400f7af64801630faae0ff14ab11c9265c0787
suvaryanserhiy <suvaryanserhiy.ua@gmail.com>
2024-02-20 18:18:40 +0100

    added navbar to index with css and js

---
+++ script/indexScript.js
@@ -0,0 +1,38 @@
+let util = {
+    mobileMenu() {
+        $("#nav").toggleClass("nav-visible");
+    },
+    windowResize() {
+        if ($(window).width() > 800) {
+            $("#nav").removeClass("nav-visible");
+        }
+    },
+    scrollEvent() {
+        let scrollPosition = $(document).scrollTop();
+
+        $.each(util.scrollMenuIds, function (i) {
+            let link = util.scrollMenuIds[i],
+                container = $(link).attr("href"),
+                containerElement = $(container);
+
+            if (containerElement.length) {
+                let containerOffset = containerElement.offset().top,
+                    containerHeight = containerElement.outerHeight(),
+                    containerBottom = containerOffset + containerHeight;
+
+                if (scrollPosition === containerBottom - 20 || (scrollPosition >= containerOffset - 20 && scrollPosition < containerBottom - 20)) {
+                    $(link).addClass("active");
+                } else {
+                    $(link).removeClass("active");
+                }
+            }
+        });
+    }
+};
+
+$(document).ready(function() {
+    util.scrollMenuIds = $("a.nav-link[href]");
+    $("#menu").click(util.mobileMenu);
+    $(window).resize(util.windowResize);
+    $(document).scroll(util.scrollEvent);
+});
--- views/index.php
+++ views/index.php
@@ -1,29 +1,46 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>simpleBlog</title>
+    <link rel="stylesheet" href="../style/indexStyle.css">
+    <title>SimpleBlog</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="../script/indexScript.js"></script>
 </head>
+
 <body>
-<div class="mainWrapper">
-    <header>
-        <div id="headerRow">
-            <div class="headerLeft">
-                <img src='/img/logo.png' alt="logoImg">
-            </div>
-            <div class="headerRight">
-                <ul>
-                    <li>HOME</li>
-                    <li>BLOG</li>
-                    <li>CONTACTS</li>
-                </ul>
-            </div>
+<header id="nav-wrapper">
+    <nav id="nav">
+        <div class="nav left">
+            <span class="gradient skew"><h1 class="logo un-skew"><a href="#home">simpleBlog</a></h1></span>
+            <button id="menu" class="btn-nav"><span class="fas fa-bars"></span></button>
         </div>
-    </header>
-</div>
+        <div class="nav right">
+            <a href="#home" class="nav-link active"><span class="nav-link-span"><span class="u-nav">Home</span></span></a>
+            <a href="#blogs" class="nav-link"><span class="nav-link-span"><span class="u-nav">Blogs</span></span></a>
+            <a href="#about" class="nav-link"><span class="nav-link-span"><span class="u-nav">About</span></span></a>
+            <a href="#contact" class="nav-link"><span class="nav-link-span"><span class="u-nav">Contact</span></span></a>
+        </div>
+    </nav>
+</header>
+<main>
+    <section id="home">
+
+    </section>
+    <section id="blogs">
+
+    </section>
+    <section id="about">
+
+    </section>
+    <section id="contact">
+
+    </section>
+</main>
+
 </body>
+
 </html>
-<?php
